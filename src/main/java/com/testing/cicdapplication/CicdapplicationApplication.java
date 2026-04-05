66230f6f99830efaeca0c71dfe94c1d74660d3af
Deepak <bhardwaj2008@gmail.com>
2026-04-05 13:36:19 +0530

    initial commit

---
+++ src/main/java/com/testing/cicdapplication/CicdapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.testing.cicdapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testing/cicdapplication/CicdapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testing.cicdapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
